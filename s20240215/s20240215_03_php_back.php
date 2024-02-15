561b57cf817c692ae457fad48fcb423c9d1770b5
dean5201314 <smile90624@gmail.com>
2024-02-15 10:49:41 +0800

    fix url

--- s20240215/s20240215_03_php_back.php
+++ s20240215/s20240215_03_php_back.php
@@ -1,10 +1,11 @@
-<?php 
-    $data = [
-        's1' => 'amy',
-        's2' => 'bob',
-        's3' => 'cat'
+<?php
+// header('Access-Control-Allow-Origin: *');
+// header('Content-Type: application/json');
 
-    ];
+$data = [
+    's1' => 'amy',
+    's2' => 'bob',
+    's3' => 'cat'
+];
 
-    echo json_encode($data);
-?>
+echo json_encode($data);
